3b849490a2464931f3496c49c752c1471cd045e2
Jamie Davies <jamie@jagd.me>
2021-05-02 21:03:53 +0100

    Add terraform for creating S3 bucket to store terraform state

---
+++ terraform/aws-terraform-backend/main.tf
@@ -0,0 +1,44 @@
+terraform {
+  required_version = ">= 0.15.0"
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 3.0"
+    }
+  }
+}
+
+provider "aws" {
+  region = "eu-west-2"
+}
+
+resource "aws_kms_key" "state" {
+  deletion_window_in_days = "20"
+}
+
+resource "aws_s3_bucket" "state" {
+  bucket = "jagd-ops-terraform-remote-state"
+  acl    = "private"
+
+  versioning {
+    enabled = true
+  }
+
+  server_side_encryption_configuration {
+    rule {
+      apply_server_side_encryption_by_default {
+        kms_master_key_id = aws_kms_key.state.arn
+        sse_algorithm     = "aws:kms"
+      }
+    }
+  }
+}
+
+resource "aws_s3_bucket_public_access_block" "state" {
+  bucket                  = aws_s3_bucket.state.id
+  block_public_acls       = true
+  block_public_policy     = true
+  ignore_public_acls      = true
+  restrict_public_buckets = true
+}
