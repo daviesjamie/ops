18568f37dd859bbee87c518b41e127986158cb06
Jamie Davies <jamie@jagd.me>
2021-05-02 21:18:24 +0100

    Store digitalocean terraform state in S3

--- terraform/digitalocean/provider.tf
+++ terraform/digitalocean/provider.tf
@@ -1,3 +1,21 @@
+terraform {
+  required_version = ">= 0.15"
+
+  required_providers {
+    digitalocean = {
+      source  = "digitalocean/digitalocean"
+      version = "2.8.0"
+    }
+  }
+
+  backend "s3" {
+    key     = "infra/digitalocean.tfstate"
+    bucket  = "jagd-ops-terraform-remote-state"
+    region  = "eu-west-2"
+    encrypt = true
+  }
+}
+
 provider "digitalocean" {
   token = var.do_token
 }
