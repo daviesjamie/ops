bffe75c47ec377d4a06c1176f1f668ddbb457a94
Jamie Davies <jamie@jagd.me>
2021-07-25 20:14:08 +0100

    Add detail to DigitalOcean terraform variables

--- terraform/digitalocean/variables.tf
+++ terraform/digitalocean/variables.tf
@@ -1,2 +1,9 @@
-variable "do_ssh_key_name" {}
-variable "do_token" {}
+variable "do_ssh_key_name" {
+  description = "Name of the SSH key to add to the droplets"
+  type = string
+}
+
+variable "do_token" {
+  description = "Personal access token for accessing the DigitalOcean API"
+  type = string
+}
