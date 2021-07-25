a7775e5861f8ef0e95af9305ee5f1b25efec090c
Jamie Davies <jamie@jagd.me>
2021-07-25 21:27:35 +0100

    Add IP addresses of droplets as outputs

--- terraform/digitalocean/droplets.tf
+++ terraform/digitalocean/droplets.tf
@@ -17,3 +17,13 @@ resource "digitalocean_droplet" "nutmeg" {
   size     = "s-1vcpu-1gb"
   ssh_keys = [data.digitalocean_ssh_key.ssh_key.fingerprint]
 }
+
+output "cumin_ipv4_address" {
+  description = "The IPv4 address of the cumin droplet"
+  value = digitalocean_droplet.cumin.ipv4_address
+}
+
+output "nutmeg_ipv4_address" {
+  description = "The IPv4 address of the nutmeg droplet"
+  value = digitalocean_droplet.nutmeg.ipv4_address
+}
