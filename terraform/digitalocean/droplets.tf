49b25639cef109fd39cec58c17575af503c2ecaa
Jamie Davies <jamie@jagd.me>
2021-07-25 20:14:31 +0100

    Create two new droplets, cumin and nutmeg

--- terraform/digitalocean/droplets.tf
+++ terraform/digitalocean/droplets.tf
@@ -2,9 +2,17 @@ data "digitalocean_ssh_key" "ssh_key" {
   name = var.do_ssh_key_name
 }
 
-resource "digitalocean_droplet" "web" {
+resource "digitalocean_droplet" "cumin" {
   image    = "ubuntu-20-04-x64"
-  name     = "paprika"
+  name     = "cumin"
+  region   = "lon1"
+  size     = "s-1vcpu-1gb"
+  ssh_keys = [data.digitalocean_ssh_key.ssh_key.fingerprint]
+}
+
+resource "digitalocean_droplet" "nutmeg" {
+  image    = "ubuntu-20-04-x64"
+  name     = "nutmeg"
   region   = "lon1"
   size     = "s-1vcpu-1gb"
   ssh_keys = [data.digitalocean_ssh_key.ssh_key.fingerprint]
